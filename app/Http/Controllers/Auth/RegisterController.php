b9a20852687ff6baf9ed347cfbe67ad0aeef8f9c
Ademola Plumptre <dplumptre1@gmail.com>
2021-11-01 13:37:00 +0100

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -76,6 +76,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role_id' => 1,
         ]);
     }
 }
