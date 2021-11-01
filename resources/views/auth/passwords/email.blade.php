1c12bf40b1521a68e65e04f4f79500b863858019
Ademola Plumptre <dplumptre1@gmail.com>
2021-11-01 15:42:07 +0100

    update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,7 +7,7 @@
                 <div class="card">
 
 
-                    <div class="card-body">
+                    <div class="card-body" style="margin:60px">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card" style="margin:50px">
+                   
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="margin:60px">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
@@ -16,6 +17,7 @@
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
                     {{ __('If you did not receive the email') }},
+                    
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
