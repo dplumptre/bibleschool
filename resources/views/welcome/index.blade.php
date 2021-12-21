cc01a94c8d5cc52b8f972d46621d6ead45eafb98
Ademola Plumptre <dplumptre1@gmail.com>
2021-12-21 16:40:33 +0100

    update

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -64,7 +64,6 @@
                     <div class="becomea_instructor_home3 style1">
                         <div class="bi_grid">
                             <h3>SCHOOL OF POST GRADUATE STUDIES</h3>
-                            <p>Teach what you love. Dove Schooll gives you the tools to create an <br class="dn-lg"> online course.</p>
                             <a class="btn btn-dark" href="{{ asset('school-of-post-graduate-studies')}}">Start Now <span class="flaticon-right-arrow-1"></span></a>
                         </div>
                     </div>
@@ -73,7 +72,6 @@
                     <div class="becomea_instructor_home3 style2">
                         <div class="bi_grid">
                             <h3>SCHOOL OF MINISTRY</h3>
-                            <p>Get unlimited access to 2,500 of Udemy’s top courses for <br class="dn-lg"> your team.</p>
                             <a class="btn btn-thm2" href="{{ asset('school-of-ministry')}}">Start Now <span class="flaticon-right-arrow-1"></span></a>
                         </div>
                     </div>
